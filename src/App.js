f67250c6ae20a23a3847ba45718124b1a6cd92b2
Siowyx <yeexian.siow@yale.edu>
2023-07-11 12:19:17 -0400

    added squash floor img and marking functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import squashfloor from "./squash-floor.png";
 
 function App() {
+  const [markers, setMarkers] = useState([]);
+  // const [imgDim, setImgDim] = useState([])
+
+  const mark = (e) => {
+    // console.log(e);
+    const offX = e.target.offsetParent.offsetLeft;
+    const offY = e.target.offsetParent.offsetTop;
+    setMarkers([...markers, [e.clientX - offX, e.clientY - offY]]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <div id="canvas-container">
+          <img id="canvas-img" src={squashfloor} onClick={mark} />
+          <div className="markers-container">
+            {markers.map((marker, index) => {
+              return (
+                <p
+                  key={index}
+                  style={{ left: marker[0] - 3, top: marker[1] - 8 }}
+                >
+                  &bull;
+                  {/* {marker[0]}, {marker[1]} */}
+                </p>
+              );
+            })}
+          </div>
+        </div>
+
+        {/* <button className="reset-btn">reset</button> */}
+      </section>
+    </main>
   );
 }
 
