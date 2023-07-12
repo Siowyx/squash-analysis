5faab1988569aea2ddcda46446707ace44c790d5
Siowyx <yeexian.siow@yale.edu>
2023-07-12 19:53:33 -0400

    added counter, undo, reset, local storage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
-// import SquashFloor from "./SquashFloor";
+import React, { useState, useEffect } from "react";
 import squashfloor from "./squash-floor.png";
 
+const getLocalStorage = () => {
+  let markers = localStorage.getItem("markers");
+  if (markers) {
+    return JSON.parse(markers);
+  } else {
+    return [];
+  }
+};
+
 function App() {
-  const [markers, setMarkers] = useState([]);
-  const [count, setCount] = useState(0);
+  const [markers, setMarkers] = useState(getLocalStorage());
+  const [count, setCount] = useState(markers.length);
   // const [imgDim, setImgDim] = useState([])
+
+  useEffect(() => {
+    localStorage.setItem("markers", JSON.stringify(markers));
+    setCount(markers.length);
+  }, [markers]);
 
   const mark = (e) => {
     // console.log(e);
     const offX = e.target.offsetParent.offsetLeft;
     const offY = e.target.offsetParent.offsetTop;
     setMarkers([...markers, [e.pageX - offX, e.pageY - offY]]);
+    setCount(count + 1);
   };
 
   return (
@@ -36,8 +50,26 @@ function App() {
             })}
           </div>
         </div>
-
-        {/* <button className="reset-btn">reset</button> */}
+      </section>
+      <section className="info-container">
+        <h3 style={{ display: "inline" }}>{count}</h3>{" "}
+        <p style={{ display: "inline" }}>shots marked</p>
+        <button
+          className="undo btn"
+          onClick={() => {
+            setMarkers(markers.slice(0, -1));
+          }}
+        >
+          undo
+        </button>
+        <button
+          className="reset btn"
+          onClick={() => {
+            setMarkers([]);
+          }}
+        >
+          reset
+        </button>
       </section>
     </main>
 
