5d41a47aa5d0f5d7980332ed6b2ac2e02ed69fea
Siowyx <yeexian.siow@yale.edu>
2023-07-12 19:05:40 -0400

    allow overlapping markers

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React, { useState } from "react";
+import SquashFloor from "./SquashFloor";
 import squashfloor from "./squash-floor.png";
 
 function App() {
   const [markers, setMarkers] = useState([]);
+  const [count, setCount] = useState(0);
   // const [imgDim, setImgDim] = useState([])
 
   const mark = (e) => {
     // console.log(e);
     const offX = e.target.offsetParent.offsetLeft;
     const offY = e.target.offsetParent.offsetTop;
-    setMarkers([...markers, [e.clientX - offX, e.clientY - offY]]);
+    setMarkers([...markers, [e.pageX - offX, e.pageY - offY]]);
   };
 
   return (
     <main>
       <section className="container">
         <div id="canvas-container">
-          <img id="canvas-img" src={squashfloor} onClick={mark} />
+          <img id="canvas-img" src={squashfloor} />
+          <div className="click-surface" onClick={mark}></div>
           <div className="markers-container">
             {markers.map((marker, index) => {
               return (
-                <p
+                <span
                   key={index}
-                  style={{ left: marker[0] - 3, top: marker[1] - 8 }}
+                  style={{
+                    left: marker[0] - 3,
+                    top: marker[1] - 10,
+                  }}
                 >
-                  &bull;
-                  {/* {marker[0]}, {marker[1]} */}
-                </p>
+                  &#x25cf;
+                </span>
               );
             })}
           </div>
@@ -35,6 +40,8 @@ function App() {
         {/* <button className="reset-btn">reset</button> */}
       </section>
     </main>
+
+    // <SquashFloor />
   );
 }
 
